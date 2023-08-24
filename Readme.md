ec9266b2e379b23c44ea16001de27c533f5cc3ee
Naischasuriel <naishasuriel24@gmail.com>
2023-08-24 09:07:19 -0400

    second push

---
+++ Readme.md
@@ -0,0 +1,51 @@
+# Guess The Sneaker
+
+
+
+## Project Description 
+
+My game is Guess the Sneaker , a game that challenges your memory and logic. The game is played on a 4x4 grid each card is displayed faced down and randomized. You have to choose two cards of the 16 and match them . There is a 20 second timer you have to match the cards within those 30 seconds.You can only flip two cards at a time. 
+
+
+## Technologies Used
+
+Index HTML
+Main.Js
+Main.Css     
+
+
+## Screenshots
+
+Game at start.
+
+![start image](/screenshots/before%20game%20start.png)
+
+Game after loosing.
+![finish image](/screenshots/after%20losing.png)
+
+
+
+
+
+
+
+
+
+
+
+
+## Getting Started
+You can play this by clicking this link:
+
+## Game Controls 
+Click on any two cards.
+
+
+## Next steps 
+Add background music to the game.
+Give the player a limited amount of moves. 
+Increase difficulty level.
+
+
+## Sources
+Used Google images for the Sneaker images.
--- script.js
+++ script.js
@@ -1,65 +1,99 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
 
-let matched = 0;
-let cardOne, cardTwo;
+let maxTime = 30;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
 let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
 
-function flipCard({ target: clickedCard }) {
-  if (cardOne !== clickedCard && !disableDeck) {
-    clickedCard.classList.add("flip");
-    if (!cardOne) {
-      return cardOne = clickedCard;
+function initTimer() {
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
     }
-    cardTwo = clickedCard;
-    disableDeck = true;
-    let cardOneImg = cardOne.querySelector(".back-view img").src,
-      cardTwoImg = cardTwo.querySelector(".back-view img").src;
-    matchCards(cardOneImg, cardTwoImg);
-  }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+    if (timeLeft === 0) {
+        alert('Times Up')
+    }
+}
+
+function flipCard({target: clickedCard}) {
+    if(!isPlaying) {
+        isPlaying = true;
+        timer = setInterval(initTimer, 1000);
+    }
+    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+        flips++;
+        flipsTag.innerText = flips;
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
 }
 
 function matchCards(img1, img2) {
-  if (img1 === img2) {
-    matched++;
-    if (matched == 8) {
-      setTimeout(() => {
-        return shuffleCard();
-      }, 1000);
+    if(img1 === img2) {
+        matchedCard++;
+        if(matchedCard === 6 && timeLeft > 0) {
+            alert('You Win')
+            return clearInterval(timer);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
     }
-    cardOne.removeEventListener("click", flipCard);
-    cardTwo.removeEventListener("click", flipCard);
-    cardOne = cardTwo = "";
-    return disableDeck = false;
-  }
-  setTimeout(() => {
-    cardOne.classList.add("shake");
-    cardTwo.classList.add("shake");
-  }, 400);
 
-  setTimeout(() => {
-    cardOne.classList.remove( "flip");
-    cardTwo.classList.remove( "flip");
-    cardOne = cardTwo = "";
-    disableDeck = false;
-  }, 1200);
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
 }
 
 function shuffleCard() {
-  matched = 0;
-  disableDeck = false;
-  cardOne = cardTwo = "";
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-  arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-  cards.forEach((card, i) => {
-    card.classList.remove("flip");
-    let imgTag = card.querySelector(".back-view img");
-    // imgTag.src = `images/img-${arr[i]}.png`;
-    card.addEventListener("click", flipCard);
-  });
+    timeLeft = maxTime;
+    flips = matchedCard = 0;
+    cardOne = cardTwo = "";
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
+
+    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        setTimeout(() => {
+            //imgTag.src = `images/img-${arr[index]}.png`;
+        }, 500);
+        card.addEventListener("click", flipCard);
+    });
 }
 
 shuffleCard();
 
+refreshBtn.addEventListener("click", shuffleCard);
+
 cards.forEach(card => {
-  card.addEventListener("click", flipCard);
+    card.addEventListener("click", flipCard);
 });
